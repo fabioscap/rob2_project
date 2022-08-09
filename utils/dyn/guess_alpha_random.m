165347d4af7c0e95f9b54e54c8b16f1a564cdc3e
fabioscap <fabiovt_99@hotmail.it>
2022-08-09 17:02:29 +0200

    updated guess alpha random

--- utils/dyn/guess_alpha_random.m
+++ utils/dyn/guess_alpha_random.m
@@ -1,19 +1,21 @@
-function alpha = guess_alpha_random(q,gr,tries)
+function [alpha,q_] = guess_alpha_random(q,gr,tries)
 % q symbolic vector
 % gr gravity term
 
 j = jacobian(gr,q);
 JJ = simplify(j'*j);
 alpha = 0;
+q_ = [0;0;0];
 for i=1:tries
     q_sampled = 2*pi*rand(3,1);
     JJ_ = eval(subs(JJ,q,q_sampled));
     new_alpha = sqrt(max(eig(JJ_)));
     if new_alpha > alpha
         alpha = new_alpha;
+        q_ = q_sampled;
     end
 
 end
 
-end
+end 
 
